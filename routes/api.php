5b363ca9f3889f29572f3aed03a829c5b9c9593f
Kyle Ross <kyle@ktross.com>
2018-10-30 03:27:09 -0400

    Fix API routes typo

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 // API is read-only, therefore we only use index and show methods
 Route::resource('users', 'UserController')->only(['index', 'show']);
-Route::resource('posts', 'ProjectController')->only(['index', 'show']);
+Route::resource('posts', 'PostController')->only(['index', 'show']);
 Route::resource('projects', 'ProjectController')->only(['index', 'show']);
 
 // todo: why can't I use only() on Route::resources (plural)?
